efa4bd132a14bf5e9a1ecaa6ff205a7075bf1206
Wanner <>
2021-04-20 16:49:00 +0000

    Cambie el char por un string

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
  {
    int A,H,B;
-   char W [10];
-   cout << "Digite su nombre"<< endl<< W;
+   string W ;
+   cout << "Digite su nombre: "<< endl<< W;
    cin >> W;
    cout << W << " Con este programa encontrara el area del triangulo" <<endl;
    cout << "Digite el valor de la base"<< endl;
