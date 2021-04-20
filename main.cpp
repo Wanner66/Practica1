407b02a8625e748b32e24ea31ddd7db6d3af8b1c
Wanner <>
2021-04-20 13:40:43 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+   int A,H,B;
+   char W [10];
+   cout << "Digite su nombre"<< endl<< W;
+   cin >> W;
+   cout << W << " Con este programa encontrara el area del triangulo" <<endl;
+   cout << "Digite el valor de la base"<< endl;
+   cin >> B;
+   cout << "Digite el valor de la Altura"<< endl;
+   cin >> H;
+   A = (B*H)/2;
+   cout << W << " El Area del triangulo es: "<< A;
+
+ }
